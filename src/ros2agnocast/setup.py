313f68d97cd14d00114611d2a6327de4932fac5c
Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
2026-05-21 08:51:26 +0900

    add ros2 topic echo_agnocast command

--- src/ros2agnocast/setup.py
+++ src/ros2agnocast/setup.py
@@ -32,6 +32,7 @@ setup(
             'list_agnocast = ros2agnocast.verb.list_agnocast:ListAgnocastVerb',
             'info_agnocast = ros2agnocast.verb.topic_info_agnocast:TopicInfoAgnocastVerb',
             'hz_agnocast = ros2agnocast.verb.topic_hz_agnocast:TopicHzAgnocastVerb',
+            'echo_agnocast = ros2agnocast.verb.topic_echo_agnocast:TopicEchoAgnocastVerb',
         ],
         'ros2node.verb': [
             'list_agnocast = ros2agnocast.verb.node_list_agnocast:ListAgnocastVerb',
